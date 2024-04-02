40afb11cd071499abbeec9f6515f228f2dcaaed1
Bryan Ang <bryan.ang>
2024-04-02 16:58:04 -0700

    reconfigured cors

--- app.js
+++ app.js
@@ -7,19 +7,7 @@ import "dotenv/config";
 const app = express()
 app.use(express.json());
 app.use(
-    cors({
-        credentials: true, // support cookies
-        origin: function (origin, callback) {
-            // callback to set origin dynamically. for supporting multiple endpoints
-            if (!origin) return callback(null, true);
-            // split env var into multiple endpoints and check each
-            if (process.env.FRONTEND_URLS.split(" ").indexOf(origin) === -1) {
-                var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        }
-    })
+    cors()
 );
 
 app.get('/testresponse', (req, res) => {
